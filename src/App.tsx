52d6a2be9e5fc1d359e99b3e1f848a7e6a1a9667
caliel <caliel1023@yahoo.com>
2023-11-19 12:22:24 -0300

    feat: company form with react hook form

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CadastroDepartamento from './components/Modals/CadastroDepartamento';
+import CadastroEmpresa from './components/cadastroEmpresa/CadastroEmpresa'
 
 function App() {
   return (
     <div className="App">
       <CadastroDepartamento/>
+      <CadastroEmpresa/>
     </div>
   );
 }
---
+++ src/components/cadastroEmpresa/CadastroEmpresa.tsx
@@ -0,0 +1,56 @@
+import { useForm } from "react-hook-form"
+import './CadastroEmpresa.css'
+
+const CadastroDepartamento = () => {
+
+    const {register , handleSubmit , reset} = useForm();
+
+    const handleSubmitData = (data: any) =>{
+        console.log('submit' , data)
+    }
+    
+
+  return (
+    <div>
+
+        <div className="container">
+          <div className="card">
+
+          
+        
+        <form onSubmit={handleSubmit(handleSubmitData)}>
+
+          <div className="closeModal">
+            <button type='reset' onClick={reset}>x</button>
+          </div>
+            <div >
+            <input {...register('nomeEmpresa')}  type="text" placeholder="Nome do Empresa" required/>
+            </div>
+
+            <div>
+            <input {...register('ceo')} type="text" placeholder="Ceo" required/>
+            </div>
+
+            <div>
+            <input {...register('cnpj')} type="text" placeholder="CNPJ" required/>
+            </div>
+
+            <div>
+            <input {...register('nicho')} placeholder="nicho" required/>
+            </div>
+
+            <div>
+            <input type='date' {...register('date')} placeholder="data de criação" required/>
+            </div>
+
+            <div>
+            <input	type="submit" className="submit"/>
+            </div>
+        </form>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default CadastroDepartamento
