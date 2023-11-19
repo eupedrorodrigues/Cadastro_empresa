c06534c212dcc6ede9fbc47f71115b0b462075e2
caliel <caliel1023@yahoo.com>
2023-11-19 13:51:44 -0300

    feat: employee form with reack hook form

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,12 @@ import './App.css';
 import CadastroDepartamento from './components/Modals/CadastroDepartamento';
 import CadastroEmpresa from './components/cadastroEmpresa/CadastroEmpresa'
 import TasksForm from './components/tasks/TasksForm';
+import CadastroFuncionario from './components/CadastroFuncionario/CadastroFuncionario';
 
 function App() {
   return (
     <div className="App">
-      <TasksForm/>
+      <CadastroFuncionario/>
     </div>
   );
 }
